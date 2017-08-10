3be24ea13f183833b4d031de46f164451587cf64
Shubham soni <shubhamsoni136@gmail.com>
2017-08-10 11:56:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter', function (req , res){
     counter++;
-    res.end(counter.toString());
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/profile' , function(req,res){
+  res.sendFile(path.join(__dirname, 'ui','profile.html'));  
 });
 
 app.get('/ui/madi.png', function (req, res) {
