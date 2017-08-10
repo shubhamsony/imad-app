cb68b945bb2e156beb383df30fd96f322e411672
Shubham soni <shubhamsoni136@gmail.com>
2017-08-11 00:09:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,15 +3,37 @@ button.onclick = function(){
     
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-      if(request.readyState=== XMLHttpRequest.DONE){
-          if(request.status===200){
-              var counter= request.responseText;
-              var countt = document.getElementById('count');
-              countt.innerHTML=counter.toString();
-          }
-      }
+        if(request.readyState=== XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter= request.responseText;
+                var countt = document.getElementById('count');
+                countt.innerHTML=counter.toString();
+            }
+         }
     };
     
     request.open('GET', 'http://shubhamsoni136.imad.hasura-app.io/counter',true);
     request.send('null');
+};
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    var rqst = new XMLHttpRequest();
+    rqst.onreadystatechange = function(){
+       if(rqst.readystate===XMLHttpRequest.DONE){
+            if(rqst.status===200){
+                var namestring = rqst.responseText;
+                var names1=JSON.parse(namestring);
+                var newname ="";
+                for(var i=0; i<names1.length;i++){
+                    newname+='<li>'+names1[i]+'</li>';
+                }
+                var newlist = document.getElementById('newlist');
+                newlist.innerHTML=newname;
+            }        
+        }
+    };
+    var nameinput=document.getElementById('name');
+    var namelist = nameinput.value;
+    rqst.open('GET','http://shubhamsoni136.imad.hasura-app.io/submit-name?'+namelist , true );
+    rqst.send('null');
 };
