8d11aed4693818fc5899c81a1663fc49bbbce134
Shubham soni <shubhamsoni136@gmail.com>
2017-08-15 03:35:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/article/:articleName', function (req, res) {
-  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'",function(err,result){
+  pool.query("SELECT * FROM article WHERE title =$1 " , [req.params.articleName],function(err,result){
    if(err){
        res.status(500).send(err.toString());
    } else{
