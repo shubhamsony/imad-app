bb12eb532f4a59a106559af8c24fe4b2ed5be179
Shubham soni <shubhamsoni136@gmail.com>
2017-08-15 01:42:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/counter', function (req , res){
 
 app.get('/testdb' , function (req , res){
     pool.query('SELECT * FROM test' , function(err , result){
-        res.send('shubham');
+        res.send('result');
     });
 });
 app.get('/ui/style.css', function (req, res) {
