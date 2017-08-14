7b206309ba60b540b80d3ba8f9f165af4e366654
Shubham soni <shubhamsoni136@gmail.com>
2017-08-15 02:22:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database:"shubhamsoni136",
     host:"db.imad.hasura-app.io",
     port:"5432",
-    password:process.env.DB_PASSWORd
+    password:process.env.DB_PASSWORD
 }; 
 var pool = new Pool(config);
 var app = express();
