7fe550a5c54497bcfe760266ac982070e9e95b51
Shubham soni <shubhamsoni136@gmail.com>
2017-08-21 00:48:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function createTemplate(data){
             <link rel="stylesheet" href="/ui/style.css">
         </head>
         <body>
-            <div class="container">
+            <div class="container center">
                 <div>
                     <a href="/">home</a>
                 </div>
