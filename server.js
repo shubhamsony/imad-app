2323b8d330408a57fc93474c232a12b59ac5cf1f
Shubham soni <shubhamsoni136@gmail.com>
2017-08-23 03:35:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,6 @@ app.get('/check-login' , function(req , res){
 
 app.get('/logout' , function(req , res){
     delete req.session.auth;
-    alert('logged out successfully');
     res.redirect('/');
 });
 
