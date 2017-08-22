c8c762540443fe3dc4678cbd4fb8c6891e767f77
Shubham soni <shubhamsoni136@gmail.com>
2017-08-22 21:21:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ function hash(input, salt ){
 app.post('/login', function(req,res){
     var username=req.body.username;
     var password=req.body.password;
-    pool.query('SELECT FROM test WHERE username=$1',[username],function(err,result){
+    pool.query('SELECT * FROM test WHERE username=$1',[username],function(err,result){
         if(err){
             res.status(500).send();
         }else{
