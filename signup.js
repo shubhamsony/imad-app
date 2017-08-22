f5c3d9d4be460a2c528d76da73b1dade54db9263
Shubham soni <shubhamsoni136@gmail.com>
2017-08-23 01:31:51 +0530

    [imad-console] Updates signup.js

--- signup.js
+++ signup.js
@@ -6,16 +6,14 @@ submit.onclick = function(){
        if(request.readyState === XMLHttpRequest.DONE){
             if(request.status===200){
                 alert('logged in successfully');
-            } else if(request.status===403){
-                alert('username/password incorrect');
-            } else if(request.status===500){
+            }else if(request.status===500){
                 alert('something went wrong on the server');
             }
         }
     };
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;
-    request.open('POST','http://shubhamsoni136.imad.hasura-app.io/login', true );
+    request.open('POST','http://shubhamsoni136.imad.hasura-app.io/create-user', true );
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({"username":username , "password":password }));
 };
