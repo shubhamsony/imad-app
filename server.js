37036ef508addff930e386a58a3c25a1a4b58d66
Shubham soni <shubhamsoni136@gmail.com>
2017-08-23 03:37:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,6 @@ app.get('/check-login' , function(req , res){
         res.send('you are logged in as'+req.session.auth.userId);
     }else{
         res.send('you are not logged in');
-        res.redirect('/');
     }
 });
 
