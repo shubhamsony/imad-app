9ca5d5e1def1dfe4f9b5856fa75088b1d154a224
Shubham soni <shubhamsoni136@gmail.com>
2017-08-23 01:09:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,23 @@ submit.onclick = function(){
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({"username":username , "password":password }));
 };
+
+
+var submit = document.getElementById('submit_btn2');
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                alert('user created successfully successfully');
+            } else if(request.status===500){
+                alert('something went wrong on the server');
+            }
+        }
+    };
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    request.open('POST','http://shubhamsoni136.imad.hasura-app.io/create-user', true );
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({"username":username , "password":password }));
+};
