49b8d11079e00ecfb54b2aff6c4557fc7c8b18ef
Shubham soni <shubhamsoni136@gmail.com>
2017-08-26 05:23:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,4 @@
 
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-       if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status===200){
-                alert('logged in successfully');
-            } else if(request.status===403){
-                alert('username/password incorrect');
-            } else if(request.status===500){
-                alert('something went wrong on the server');
-            }
-        }
-    };
-    var username=document.getElementById('username').value;
-    var password=document.getElementById('password').value;
-    request.open('POST','http://shubhamsoni136.imad.hasura-app.io/login', true );
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({"username":username , "password":password }));
-};
 
 function LoadLoginForm(){
     var loginHtml=`
@@ -26,7 +6,86 @@ function LoadLoginForm(){
       <h2>Login to continue</h2>
       <input type="text" id="username" placeholder="username"/>
       <input type="password" id="password"/>
-      <input type="submit" value="Submit" id="submit_btn"/>
+      <input type="submit" value="Login" id="submit_btn"/>
       `;
-      document.getElementById('login_area').innerHTML=LoginHtml;
+    document.getElementById('login_area').innerHTML=LoginHtml;
+      
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                submit.value='Success!';
+            } else if(request.status===403){
+                alert('username/password incorrect');
+                submit.value='Login';
+            } else if(request.status===500){
+                alert('something went wrong on the server');
+                submit.value='Login';
+            }else{
+                submit.value='Login';
+            }
+            loadLogin();
+        }
+    };
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    request.open('POST','http://shubhamsoni136.imad.hasura-app.io/login', true );
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({"username":username , "password":password }));
+    submit.value="Logging In";
+};
+
 }
+function loadLoggedInUser (username){
+    var loginarea = document.getElementById('login_area');
+    loginarea.innerHTML=`
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+        `;
+        
+}
+
+function loadLogin(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                loadLoggedInUser(this.responseText);
+                
+            }else{
+                loadLoginForm();
+            }
+        }
+    };
+    
+    request.open('GET','/check-login',True);
+    request.send(null);
+}
+
+function loadArticles(){
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            var articles = document.getElementById('articles');
+            if(request.status===200){
+                var content='<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for(var i=0 ; i < articleData.length() ; i++){
+                    content+= `<li> <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content+='</ul>';
+                articles.innerHTML=content;
+            }else{
+                articles.innerHTML('Oops! could not load all articles');
+            }
+        }
+    };
+    request.open('GET','/get-articles',True);
+    request.send(null);
+}
+loadLogin();
+loadArticles();
