55107e38436851205799964ef306d671d6e9d9f8
Shubham soni <shubhamsoni136@gmail.com>
2017-08-26 17:15:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ app.get('/get-comments/:articleName' ,function(req,res){
               if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send(JSON.stringify(result.rows));
+          res.send(JSON.stringify(result.rows[0]));
       }
     });
 });
