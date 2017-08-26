728f74a11b40909462246434d1358ac459374813
Shubham soni <shubhamsoni136@gmail.com>
2017-08-26 05:34:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
- document.getElementById('login_area').innerHTML='mc';
+
+ var loginHtml=`
+      <h3>if not registered <a href="/sign-up">Sign Up</a></h3>
+      <h2>Login to continue</h2>
+      <input type="text" id="username" placeholder="username"/>
+      <input type="password" id="password"/>
+      <input type="submit" value="Login" id="submit_btn"/>
+      `;
+    document.getElementById('login_area').innerHTML='mc';
