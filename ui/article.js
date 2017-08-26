36d92b9bfb6b8477b8e6f5f95b6ab4149cb11580
Shubham soni <shubhamsoni136@gmail.com>
2017-08-26 18:13:20 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -33,6 +33,7 @@ function loadCommentForm () {
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
+        alert(comment);
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({"comment": comment}));  
