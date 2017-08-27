95b5da35bf6b2762f1160b14d4e20919fc875e6c
Shubham soni <shubhamsoni136@gmail.com>
2017-08-28 04:31:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/check-login' , function(req , res){
            if (err) {
               res.status(500).send(err.toString());
            } else {
-              res.send(result.rows[0].username);    
+              res.send(result.rows[0]);    
            }
        });
    } else {
