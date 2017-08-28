6df45d445463973ec1f7b3dc03d6f0493564db38
Shubham soni <shubhamsoni136@gmail.com>
2017-08-28 06:48:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,11 +134,11 @@ app.post('/login', function(req,res){
 app.get('/check-login' , function(req , res){
     if (req.session && req.session.auth && req.session.auth.userId) {
        // Load the user object
-       pool.query('SELECT * FROM test WHERE user_id = $1', [req.session.auth.userId], function (err, result) {
+       pool.query('SELECT test.username,test.profile_pic FROM test WHERE user_id = $1', [req.session.auth.userId], function (err, result) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
-              res.send(result.rows[0].username);    
+              res.send(result.rows[0]);    
            }
        });
    } else {
